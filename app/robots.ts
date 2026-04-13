6023bb2a4803c0eca731974a92df6474a1eb72c0
Nachman-Libermensh <bn8475266@gmail.com>
2026-04-13 10:53:15 +0300

    feat: set dynamic export to "force-static" in robots and sitemap files

--- app/robots.ts
+++ app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next"
 
 import { getAbsoluteUrl } from "@/lib/seo"
 
+export const dynamic = "force-static"
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
